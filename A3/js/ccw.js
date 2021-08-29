f9a9ce1f5ab716cea41bb6825a519dff1fefd81d
Chaemin-L <chaem12@kookmin.ac.kr>
2021-08-29 22:27:14 +0900

    feat: .action의 좋아요/싫어요 버튼 클릭시 색변화 기능 추가

--- A3/js/ccw.js
+++ A3/js/ccw.js
@@ -5,3 +5,14 @@ moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
 });
+
+/* 비슷한 맥락으로 좋아요와 싫어요 버튼 클릭시 색변화 유도*/
+const likeBtn = document.querySelector('.info .actions .likeBtn');
+const hateBtn = document.querySelector('.info .actions .hateBtn');
+
+likeBtn.addEventListener('click', () =>{
+  likeBtn.classList.toggle('active');
+});
+hateBtn.addEventListener('click', () =>{
+  hateBtn.classList.toggle('active');
+});
